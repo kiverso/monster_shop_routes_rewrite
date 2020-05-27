c23a28525d279b42c23b4fdc0025a865d22d067f
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-27 14:50:14 -0600

    Add users table to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    redirect_to profile_path
+    user = User.new(user_params)
+    if user.save
+      flash[:notice] = "You are now registered and logged in!"
+      redirect_to profile_path
+    else
+      flash[:notice] = ""
+    end
+    
   end
 
   def show
     @user = User.find(params[:user_id])
   end
   
-  
+  private
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
+  end
   
 end
